184b69824e4740d91645f85452bded635edc0931
mamonrabin <almamon757@gmail.com>
2026-04-25 13:24:29 +0600

    done checkout page

--- src/components/checkout/DropCartItems.tsx
+++ src/components/checkout/DropCartItems.tsx
@@ -3,35 +3,85 @@
 import { ChevronRight } from "lucide-react";
 import React, { useState } from "react";
 
+import product from "@/src/assets/products/bd1.jpg";
+import product2 from "@/src/assets/products/bd2.jpg";
+import product3 from "@/src/assets/products/bd3.jpg";
+import Image from "next/image";
+
 const DropCartItems = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="px-6">
       {/* Header */}
-      <div onClick={() => setOpen(!open)} className="cursor-pointer flex items-center justify-between font-medium">
-        <p>Apply Discount Code</p>
-        <p><ChevronRight size={18}/></p>
+      <div
+        onClick={() => setOpen(!open)}
+        className="cursor-pointer flex items-center justify-between font-medium"
+      >
+        <p>Cart Items (2)</p>
+        <p>
+          <ChevronRight size={18} />
+        </p>
       </div>
 
       {/* Animated Content */}
       <div
         className={`overflow-hidden transition-all duration-500 ${
-          open ? "max-h-40 opacity-100 mt-2" : "max-h-0 opacity-0"
+          open ? "max-h-80 opacity-100 mt-2" : "max-h-0 opacity-0"
         }`}
       >
-        <form className="flex flex-col gap-2 items-center">
-          <input
-            className="w-full border px-2 py-2 rounded outline-none"
-            type="text"
-            placeholder="Enter Coupon Code"
+       <div className="flex flex-col gap-2">
+         <div className="flex gap-2">
+          <div>
+            <Image
+            className="border rounded p-2"
+            src={product}
+            alt="product"
+            width={80}
+            height={80}
           />
-          <input
-            className="px-2 py-2 rounded bg-[#1B5DD5] border border-[#1B5DD5] hover:bg-transparent hover:text-[#1B5DD5] duration-300 text-white font-medium text-sm w-full cursor-pointer"
-            type="submit"
-            value="Apply"
+          </div>
+          <div>
+            <h2 className="text-sm font-medium">OnePlus Nord CE5 5G 8GB/256GB</h2>
+            <p className="text-sm"><span className="font-medium mt-1">Color : </span> <span>Mittar Yellow</span></p>
+            <p className="text-sm"><span className="font-medium mt-1">Quantity :</span> <span>3</span></p>
+          </div>
+        </div>
+
+         <div className="flex gap-2">
+          <div>
+            <Image
+            className="border rounded p-2"
+            src={product2}
+            alt="product"
+            width={80}
+            height={80}
           />
-        </form>
+          </div>
+          <div>
+            <h2 className="text-sm font-medium">OnePlus Nord CE5 5G 8GB/256GB</h2>
+            <p className="text-sm"><span className="font-medium mt-1">Color : </span> <span>Mittar Yellow</span></p>
+            <p className="text-sm"><span className="font-medium mt-1">Quantity :</span> <span>3</span></p>
+          </div>
+        </div>
+
+         <div className="flex gap-2">
+          <div>
+            <Image
+            className="border rounded p-2"
+            src={product3}
+            alt="product"
+            width={80}
+            height={80}
+          />
+          </div>
+          <div>
+            <h2 className="text-sm font-medium">mart 32 Frameless Smart Google TV (SEL-32G25F)</h2>
+            <p className="text-sm"><span className="font-medium mt-1">Color : </span> <span>Mittar Yellow</span></p>
+            <p className="text-sm"><span className="font-medium mt-1">Quantity :</span> <span>3</span></p>
+          </div>
+        </div>
+       </div>
       </div>
     </div>
   );
