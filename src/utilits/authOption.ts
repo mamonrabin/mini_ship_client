4ed147da886923c1b6e30b6c535e2a3f4e0e07b5
mamonrabin <almamon757@gmail.com>
2026-04-29 15:19:50 +0600

    doe

--- src/utilits/authOption.ts
+++ src/utilits/authOption.ts
@@ -4,9 +4,14 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
 };
+
+
+
+
+
